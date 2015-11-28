4565a39564f51b1b3cf46c6912b11f1286b0ae60
Geenz <geenz@geenzo.com>
2015-11-28 12:57:13 -0500

    Add @Prime31's CharacterController2D class
    Also start work on overworld stuff.  The battle screen will eventually use CharacterController2D as well for the player and enemy's soul.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float playerSpeed = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 movement = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
+		GetComponent<Prime31.CharacterController2D> ().move (movement * playerSpeed);
+	}
+}
